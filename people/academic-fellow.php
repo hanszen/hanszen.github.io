f91711c8e17724fb839e786be3e33d77f35371fb
Patrick Granahan <pjgranahan@gmail.com>
2016-05-16 17:36:03 -0500

    Reworking directory layout

---
+++ people/academic-fellow.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Lovett College</title>
+
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto|Karla' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CSS -->
+    <link href="../css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+    
+    <?php include "../header.php"; ?>
+
+    <div class="jumbotron" style="background-image:url('../img/presidents_cup.jpg'); background-position:center top">
+      <div class="container" align="center"><br><br>
+        <h1 style="letter-spacing: 3px">ACADEMIC FELLOWS</h1>
+      </div>
+    </div><br>
+
+    <div class="container" align="left">
+        <p>Each residential college has an Academic Fellows or Mentors Society, an honorary service organization dedicated to academic life in the college. Fellows or Mentors are upperclassmen who have: <br><br>
+
+          <ol>
+            <li>Proven academic achievement</li>
+            <li>College citizenship</li>
+            <li>Interest in the academic life of their College</li>
+            <li>Willingness to help other students</li>
+          </ol>
+        </p><br>
+
+        <p>As part of their charge, Fellows and Mentors Societies provide academic assistance on a regular basis through advertised review sessions, "office hours" or requested individual meetings. Two major advantages of consulting a Fellow or Mentor are: <br><br>
+
+          <ol>
+            <li>Convenience and proximity of source</li>
+            <li>The familiarity of the Fellow or Mentor with the specific course. Students should consult the list of Academic Fellows and Mentors at their respective colleges.</li>  
+          </ol>
+        </p><br>
+    </div>
+
+    <?php include "../footer.php"; ?>
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ people/rha.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Lovett College</title>
+
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto|Karla' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CSS -->
+    <link href="../css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <?php include "../header.php"; ?>
+
+    <div class="jumbotron" style="background-image:url('../img/presidents_cup.jpg'); background-position:center top">
+      <div class="container" align="center"><br><br>
+        <h1 style="letter-spacing: 3px">RICE HEALTH ADVISORS</h1>
+      </div>
+    </div><br>
+
+    <div class="container" align="left">
+        <p>RHAs are the student members who are on hand at each college to answer basic health and wellness-related questions. They act as a liaison between the Wellness Center and its associated programming throughout the semester. Regular meetings keep membership informed and allow for planning and evaluation of on-campus health education programs.</p>
+
+        <p>Rice Health Advisors also keep a first aid kit in their rooms. Students are welcome to stop by to pick up supplies, ask questions about health and wellness topics, discuss a personal problem or issue in a confidential setting, learn more about getting involved with the organization, or connect with the Student Wellbeing Office, Student Health Services or the Rice Counseling Center.</p>
+
+        <p>For more details on what RHAs do and how to become one, see the <a href="http://wellness.rice.edu/ricehealthadvisors">Rice Health Advisor page</a> on the Rice Wellbeing Center website!</p><br>
+
+        <div class="row">
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title" style="text-transform:uppercase; letter-spacing:2px">Inside the RHA Kit</h3>
+              </div>
+              <div class="panel-body">
+                <ul>
+                  <li>Acetaminophen</li>
+                  <li>Antacid</li>
+                  <li>Antibiotic ointment</li>
+                  <li>Band-Aids</li>
+                  <li>Cold compresses</li>
+                  <li>Condoms</li>
+                  <li>Cough drops</li>
+                  <li>Ear plugs</li>
+                  <li>Hydrocortisone</li>
+                  <li>Ibuprofen</li>
+                  <li>Maxi pads</li>
+                  <li>Personal lubricant</li>
+                  <li>Tampons</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-8">
+            <h3 align="center">2015-2016 Lovett RHAs</h3><br>
+            <p align="center">
+              Michael Armstrong<br>
+              Thomas Chen<br>
+              Darcy Curtis<br>
+              Christine Dobbin<br>
+              Alex Mardock<br>
+              Ronnie McLaren<br>
+              Linden Shih<br>
+            </p>
+          </div>
+        </div>
+    </div>
+
+    <?php include "../footer.php"; ?>
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
